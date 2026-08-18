db16dcc99ae17cba7606cdde428922c90b6d0a40
misaeloliveira419-jpg <misaeloliveira419@gmail.com>
2026-08-18 08:46:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3807,7 +3807,6 @@ function ajustarAlturaPainelRolagens(){
 
 }
 
-window.addEventListener(
-    "resize",
-    ajustarAlturaPainelRolagens
-);
+ajustarAlturaPainelRolagens();
+
+window.addEventListener("resize", ajustarAlturaPainelRolagens);
